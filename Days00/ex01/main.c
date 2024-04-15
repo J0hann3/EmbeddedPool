c32dec30b84f449f42d3a7b46d5c8a7bb2548280
J0hann3 <jvigny@student.42.fr>
2024-04-15 14:51:25 +0200

    feat: ex01 light LED

---
+++ Days00/ex01/main.c
@@ -0,0 +1,11 @@
+#include <avr/io.h>
+
+int main()
+{
+	DDRB = (1 << PB0);//0b00000001; //set PortB0 to listen
+	PORTB = (1 << PB0);//0b00000001; //init PortB0 output data to 1, to enabled the LED
+
+}
+
+
+
