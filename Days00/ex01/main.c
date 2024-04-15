182221bf0bbc9848641f0b0458246479b64e8316
J0hann3 <jvigny@student.42.fr>
2024-04-15 17:57:19 +0200

    fix: add infinite loop +  OR flags

--- Days00/ex01/main.c
+++ Days00/ex01/main.c
@@ -2,9 +2,10 @@
 
 int main()
 {
-	DDRB = (1 << PB0);//0b00000001; //set PortB0 to listen
-	PORTB = (1 << PB0);//0b00000001; //init PortB0 output data to 1, to enabled the LED
-
+	DDRB |= (1 << PB0);//0b00000001; //set PortB0 to listen
+	PORTB |= (1 << PB0);//0b00000001; //init PortB0 output data to 1, to enabled the LED
+	while (1) // infinite loop to run the programm
+	{}
 }
 
 
