b4d92809ffc684c333b66f5579c46d63fd9766d5
J0hann3 <jvigny@student.42.fr>
2024-04-17 19:24:33 +0200

    fix: number of cycle

--- Days01/ex00/main.c
+++ Days01/ex00/main.c
@@ -1,18 +1,18 @@
 #include <avr/io.h>
 #include "tool.h"
-#define NB_CYCLES 40
+#define NB_CYCLES 24
 
 
 int main()
 {
-	unsigned long long cpt = 0;
-	unsigned long long cpt_max = F_CPU / (2 * NB_CYCLES);
+	unsigned long cpt = 0;
+	unsigned long cpt_max = F_CPU / NB_CYCLES;
 
 	SET(DDRB, PB1);
 
 	while (1)
 	{
-		if (cpt >= cpt_max)
+		if (cpt == cpt_max)
 		{
 			TOGGLE(PORTB, PB1);
 			cpt = 0;
