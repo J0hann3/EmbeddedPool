1d81aa59ba31a554a454f68e2a6b38c2d0389d9c
J0hann3 <jvigny@student.42.fr>
2024-04-21 14:56:19 +0200

    feat: add count down slave

--- main.c
+++ main.c
@@ -2,8 +2,15 @@
 #include "i2c.h"
 #include "uart.h"
 
+#define LED_PORT PORTB
+#define LED_DDR DDRB
 #define ADDRESS 0x18
 #define BUTTON_PRESS 0x10
+#define LED_D1 0x12
+#define LED_D2 0x13
+#define LED_D3 0x14
+#define LED_D4 0x15
+#define GREEN_LED 0x16
 
 void init_slave();
 void slave_mode();
@@ -38,12 +45,51 @@ void init_LED_SW1()
 void init_slave()
 {
 	TWAR = ADDRESS << 1;
-	TWCR = (1 << TWEN) | (1 << TWEA);
+	TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT);
+}
+
+void light_countdown_slave()
+{
+	uint8_t stop = 0;
+
+	while(1)
+	{
+		if ((PIND & (1 << PD2)) == 0)
+		{
+			TWCR = (1 << TWEN) | (1 << TWINT);
+			stop = 1;
+		}
+		else
+			TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT);
+		while (!(TWCR & (1 << TWINT))) //wait for ack of the slave
+		{}
+		unsigned char c = TWDR;
+		switch (c)
+		{
+			case LED_D1:
+				uart_printhex(c);
+				TOGGLE(PORTB, PB0);
+			case LED_D2:
+				uart_printhex(c);
+				TOGGLE(PORTB, PB1);
+			case LED_D3:
+				uart_printhex(c);
+				TOGGLE(PORTB, PB2);
+			case LED_D4:
+				uart_printhex(c);
+				TOGGLE(PORTB, PB4);
+			case GREEN_LED:
+				stop = 1;
+				uart_printhex(c);
+				TOGGLE(PORTD, PD6);
+		}
+		if (stop == 1)
+			break;
+	}
 }
 
 void slave_mode()
 {
-	SET(PORTB, PB4);
 	uart_printstr("Start slave ...\r\n");
 	uint8_t is_press = 0;
 	while(1)
@@ -61,13 +107,35 @@ void slave_mode()
 		if (is_press == 1)
 			break;
 	}
-	SET(PORTB, PB2);
+	SET(PORTB, PB0);
+	init_slave();
+	while (!(TWCR & (1 << TWINT)) && TW_STATUS != TW_SR_SLA_ACK)
+	{}
+	uart_printstr("restart");
+	light_countdown_slave();
 	_delay_ms(5000);
+}
+
+uint8_t stair_leds() {
+	while (1) {
+		for (int i = 0; i < 4; i++) {
+			if(i2c_write(LED_D1 + i) == TW_MT_DATA_NACK)
+				return 1;
+			_delay_ms(500);
+		}
+		for (int i = 2; i >= 0; i--) {
+			if(i2c_write(LED_D1 + i) == TW_MT_DATA_NACK)
+				return 1;
+			_delay_ms(500);
+		}
+	}
+	if(i2c_write(GREEN_LED) == TW_MT_DATA_NACK)
+		return 1;
+	return 0;
 }
 
 void master_mode()
 {
-	SET(PORTB, PB0);
 	uart_printstr("Being matser ... \r\n");
 	i2c_start(TW_WRITE, ADDRESS);
 	while(1)	//check press button of slave
@@ -76,8 +144,11 @@ void master_mode()
 		if (status == TW_MT_DATA_NACK)
 			break ;
 	}
-	SET(PORTB, PB2);
+	SET(PORTB, PB0);
 	i2c_stop();
+	_delay_ms(100);
 	i2c_start(TW_WRITE, ADDRESS);
+	_delay_ms(1000);
+	stair_leds();
 	_delay_ms(5000);
 }
